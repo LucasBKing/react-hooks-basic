39f34a5a92299c8a1d778306861624c70497f0aa
lucasbking <ldsbarbosa@inf.ufpel.edu.br>
2019-06-19 16:28:37 -0300

    setting the files using geolocalization api

--- src/App.js
+++ src/App.js
@@ -1,40 +1,24 @@
 import React, { useState, useEffect } from 'react';
 
 export default function App() {
-  const [repositories, setRepositories] = useState([]);
-
-  useEffect( async () => {
-    const response = await fetch('https://api.github.com/users/lucasbking/repos');
-    const data = await response.json();
-
-    setRepositories(data);
-  }, []);
+  const [ location, setLocation] = useState({});
 
   useEffect(() => {
-    const filtered = repositories.filter(repo => repo.favorite);
+    const watchId = navigator.geolocation.watchPosition(handlePositionRecieved)
+    
+    // componentWillUnmount()
+    return () => navigator.geolocation.clearWatch(watchId);
+  }, []);
 
-    document.title = `Você tem ${filtered.length} favoritos`
-  }, [repositories]);
-
-  function handleFavorite(id) {
-    const newRepositories = repositories.map(repo => {
-      return repo.id === id ? { ...repo, favorite: !repo.favorite } : repo
-    });
-
-    setRepositories(newRepositories);
-  }
+  function handlePositionRecieved({ coords }) {
+    const { latitude, longitude } = coords;
+    setLocation({ latitude, longitude });
+  }  
 
   return (
     <>
-      <ul>
-        {repositories.map(repo => (
-          <li key={repo.id}>
-            {repo.name}
-            {repo.favorite && <span>(Fav)</span>}
-            <button onClick={() => handleFavorite(repo.id)}>Fav</button>
-          </li>
-        ))}
-      </ul>
+      Latitude: {location.latitude}<br />
+      Longitude: {location.longitude}
     </>
   );
 };
